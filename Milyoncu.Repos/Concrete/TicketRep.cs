120cb442393f4eb87859f9b3fe1e32aa711947f8
essracetiin <essracetiin@gmail.com>
2022-12-12 20:05:43 +0300

    ticket

--- Milyoncu.Repos/Concrete/TicketRep.cs
+++ Milyoncu.Repos/Concrete/TicketRep.cs
@@ -1,4 +1,6 @@
-﻿using Milyoncu.Core;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Milyoncu.Core;
 using Milyoncu.Dal;
 using Milyoncu.Entity.Concrete;
 using Milyoncu.Repos.Abstract;
@@ -12,9 +14,44 @@ namespace Milyoncu.Repos.Concrete
 {
     public class TicketRep<T> : BaseRepository<Ticket>, ITicketRep where T : class
     {
+        public MilyoncuContext _db;
         public TicketRep(MilyoncuContext db) : base(db)
         {
+            _db = db;
+        }
 
+        public Ticket CreateTicket(Ticket ticket)
+        {
+            _db.Set<Ticket>().Add(ticket);
+            return ticket;
+        }
+
+        public bool DeletebyTicketId(int ticketId)
+        {
+            Set().Remove(Find(ticketId));
+            return true;
+        }
+
+        public Ticket DeleteTicket(Ticket ticket)
+        {
+            _db.Set<Ticket>().Remove(ticket);
+            return ticket;
+        }
+
+        public Ticket GetTicketbyId(int ticketId)
+        {
+            return _db.Tickets.Include(t => t.Event).FirstOrDefault(t => t.Id == ticketId);
+        }
+
+        public IEnumerable<Ticket> GetTickets()
+        {
+            return _db.Tickets.Include(t => t.Event).ToList();
+        }
+
+        public Ticket UpdateTicket(Ticket ticket)
+        {
+            _db.Set<Ticket>().Update(ticket);
+            return ticket;
         }
     }
 }
