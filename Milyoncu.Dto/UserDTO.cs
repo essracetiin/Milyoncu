73825e98e9ff333e2e4d5d6b263334a4bdf569b0
GulmezBurak <burakgulmez34@gmail.com>
2022-12-13 20:14:56 +0300

    User login için UserDTO oluşturuldu.

---
+++ Milyoncu.Dto/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milyoncu.Dto
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+        public string Role { get; set; }
+        public bool Error { get; set; }
+
+    }
+}
