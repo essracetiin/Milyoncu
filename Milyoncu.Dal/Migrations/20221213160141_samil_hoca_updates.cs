7a0fee23c634a9c3903e91631cbc7e1e119d4b9b
ozcancambazogluu <ozcancambazoglu@hotmail.com>
2022-12-13 19:45:27 +0300

    Migrations

---
+++ Milyoncu.Dal/Migrations/20221213160141_samil_hoca_updates.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Milyoncu.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class samilhocaupdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_Users_UserId",
+                table: "Baskets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Wallets_WalletId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_Users_UserId",
+                table: "Baskets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Wallets_WalletId",
+                table: "Users",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_Users_UserId",
+                table: "Baskets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Wallets_WalletId",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_Users_UserId",
+                table: "Baskets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Events_EventId",
+                table: "Tickets",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Wallets_WalletId",
+                table: "Users",
+                column: "WalletId",
+                principalTable: "Wallets",
+                principalColumn: "Id");
+        }
+    }
+}
