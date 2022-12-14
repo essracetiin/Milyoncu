e0bf701a4fe096e768456b77ebeacb3d4cc3c228
GulmezBurak <burakgulmez34@gmail.com>
2022-12-14 16:37:08 +0300

    Register for Users.

--- Milyoncu.Dto/Models/UserModel.cs
+++ Milyoncu.Dto/Models/UserModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Milyoncu.Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Milyoncu.Dto.Models
 {
-    internal class UserModel
+    public class UserModel
     {
+        public User User { get; set; }
+        public string Message { get; set; }
     }
 }
