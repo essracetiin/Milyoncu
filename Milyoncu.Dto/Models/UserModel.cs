3838c1aed6fc54b8bb1146d85ae68b763e2a3fad
GulmezBurak <burakgulmez34@gmail.com>
2022-12-14 16:14:18 +0300

    models klasörü

---
+++ Milyoncu.Dto/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milyoncu.Dto.Models
+{
+    internal class UserModel
+    {
+    }
+}
